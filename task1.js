efd0522657e086c480cf4abfdb7ee07155452a71
saikumar2673 <169043240+saikumar2673@users.noreply.github.com>
2025-01-08 09:46:45 +0600

    task1 completed

---
+++ task1.js
@@ -0,0 +1,13 @@
+var v1=23;
+let v2=118;
+const v3='hello';
+console.log(v1);
+console.log(v2);
+console.log(v3);
+
+v4='world';
+var v4;
+let v5=21;
+v5=`helloworld`;
+console.log(v4);
+console.log(v5);
